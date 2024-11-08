d77ff674c7dd85a043df009a3e98fe34fd681941
Anish <anishrs@yahoo.com>
2024-11-08 11:08:01 -0600

    Ok.

--- AirBNB/backend/config/index.js
+++ AirBNB/backend/config/index.js
@@ -1,4 +1,39 @@
 // backend/config/index.js
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.json());
+
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
 module.exports = {
   environment: process.env.NODE_ENV || "development",
   port: process.env.PORT || 8000,
